729ca32f2e276f055f83be790c35b07ab401b6ee
Gerardo Keys <jotojry@icloud.com>
2019-12-03 17:45:11 -0800

    set up details page, finished courses page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import "./global.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Courses from "./components/Courses";
+import CourseDetail from "./components/CourseDetail";
 
 // Main container for routes to all components.
 function App() {
+  const [clickedIndex, setClickedIndex] = useState(null);
+
   return (
     <Router>
       <div>
         <Header />
 
         <Switch>
-          <Route exact path="/" component={Courses} />
-          {/* <Route path="/authenticated" component={AuthWithContext} />
-        <Route path="/settings" component={AuthWithContext} />
+          <Route exact path="/" render={() => <Courses setClickedIndex={setClickedIndex} />} />
+          <Route path={`/courses/${clickedIndex}`} render={() => <CourseDetail clickedIndex={clickedIndex} />} />
+        {/* <Route path="/settings" component={AuthWithContext} />
         <Route path="/signin" component={UserSignInWithContext} />
         <Route path="/signup" component={UserSignUpWithContext} />
         <Route path="/signout" component={UserSignOutWithContext} />
--- src/components/CourseButtons.jsx
+++ src/components/CourseButtons.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function CourseButtons({ data }) {
+export default function CourseButtons({ data, setClickedIndex }) {
   return (
     <>
       {data &&
         data.map(data => {
           return (
-            <div className="grid-33" key={data.id} onClick={() => console.log(data.id)}>
-              <Link to="/course_details">
+            <div className="grid-33" key={data.id} onClick={() => setClickedIndex(data.id)}>
+              <Link to={`/courses/${data.id}`}>
                 <div className="course--module course--link">
                   <h4 className="course--label">Course</h4>
                   <h3 className="course--title">{data.title}</h3>
